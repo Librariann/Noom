e3de3c6e6f1965b37433291d00eabfad49d47f6a
Librarian <okpc0306@naver.com>
2022-04-18 13:35:25 +0900

    complete section1

--- src/server.js
+++ src/server.js
@@ -1,1 +1,42 @@
-console.log("Hello World!");
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const port = 3000;
+const handleListen = () =>
+  console.log(`Listening on http://localhost:${port} start!`);
+
+//이렇게 만들필요 없고 웹소켓 서버만 원하면 웹소켓 서버만 만들면됨
+//이렇게 한 이유는 public, pug, js, css 등을 사용 하기 위함임
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
+  });
+});
+
+server.listen(port, handleListen);
